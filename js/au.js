10ac2412b113a785815632a4697719e30ae39daa
Zou <78598969+Zetrey@users.noreply.github.com>
2024-01-07 17:44:17 +0800

    Add files via upload

--- js/au.js
+++ js/au.js
@@ -1,15 +1,1 @@
-
-function bf() {
-    var audio = document.getElementById('bgm');
-    var bt = document.getElementById('playbt');
-
-    if (audio !== null) {
-        if (audio.paused) {
-            audio.play();
-            bt.innerText = "暂停";
-        } else {
-            audio.pause();
-            bt.innerText = "播放";
-        }
-    }
-}
+﻿ function bf() {     var audio = document.getElementById('bgm');     var bt = document.getElementById('playbt');      if (audio !== null) {         if (audio.paused) {             audio.play();             bt.innerText = "暂停";         } else {             audio.pause();             bt.innerText = "播放";         }     } }
