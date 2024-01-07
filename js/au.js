0d0548ea2ab48f97def337c0d2e3772e5cd75c1c
Zou <78598969+Zetrey@users.noreply.github.com>
2024-01-07 17:28:14 +0800

    Add files via upload

---
+++ js/au.js
@@ -0,0 +1,15 @@
+
+function bf() {
+    var audio = document.getElementById('bgm');
+    var bt = document.getElementById('playbt');
+
+    if (audio !== null) {
+        if (audio.paused) {
+            audio.play();
+            bt.innerText = "ÔÝÍ£";
+        } else {
+            audio.pause();
+            bt.innerText = "²¥·Å";
+        }
+    }
+}
